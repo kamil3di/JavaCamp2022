8ccd2774b98a9aa17b1622ac97e81d380b0c27af
JavaCamp2022 <kamildinleyici@hotmail.com>
2022-09-22 12:57:41 +0300

    stringDemo is changed

--- stringDemo/src/Main.java
+++ stringDemo/src/Main.java
@@ -1,12 +1,19 @@
-import java.awt.desktop.SystemEventListener;
-
 public class Main {
     public static void main(String[] args) {
+        //Common string variable
         String mesaj = "Merhabalar herkese!";
 
-        char[] karakterler = new char[5];
+        //First lesson's notes
+/*      char[] karakterler = new char[5];
         mesaj.getChars(0,4,karakterler,0);
         System.out.println(karakterler);
-        System.out.println(mesaj.indexOf("rh"));
+        System.out.println(mesaj.indexOf("rh"));*/
+
+        //Second lesson's notes
+        System.out.println(mesaj.replace(' ','-'));
+        System.out.println(mesaj.substring(0,10));
+        for (String kelime : mesaj.split(" ")){
+            System.out.println(kelime);
+        }
     }
 }
