356e318a5f7ee817ea90fb649cf8bec03dcc6eb6
JavaCamp2022 <kamildinleyici@hotmail.com>
2022-09-23 14:46:25 +0300

    ReferenceType and ValueType examples have examined

---
+++ classes/src/CustomerManager.java
@@ -0,0 +1,8 @@
+public class CustomerManager {
+    public void Add(){
+        System.out.println("Musteri eklendi");
+    }
+    public void Remove(){
+        System.out.println("Musteri cikarildi");
+    }
+}
