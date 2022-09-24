702a38ca48976eb325137c8cbbf76d6c7d1921d2
kamil3di <kamildinleyici@hotmail.com>
2022-09-25 00:29:36 +0300

    inHeritanceDemo is added

---
+++ inheritanceDemo/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Reference type helps us to utilize parent's children
+        KrediUI krediUI = new KrediUI();
+        krediUI.KrediHesapla(new TarimKrediManager());
+    }
+}
